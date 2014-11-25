0d21bb1485aadd4d53639f6f5c0d27879fd06a00
Vu Anh <brother.rain.1024@gmail.com>
2014-11-25 11:43:40 +0700

    change name of functions; add alias

--- bookmark.plugin.zsh
+++ bookmark.plugin.zsh
@@ -1,34 +1,47 @@
 # Bookmark Plugin
-# Features:
-# 	Autocomplete
-# Requirements: 
-# 	apparix
-# sudo apt-get install apparix
-# Usage:
-# 	bl	list bookmarks
-# 	bm	add a bookmark 
-# 	bt	cd to a bookmark
+
+# Author: Brother Rain
+# Date	: Nov 25, 2014
+
+# show help
+function bookmark_help {
+	echo "Usage: bt [NAME]" 
+	echo "Change directory to a bookmark\n"
+
+	bookmark_list
+}
 
 # list bookmarks
-function bl {
-	echo "Bookmarks: "
+function bookmark_list {
+	echo "Available bookmarks: \n"
 	apparix | sed -n '4,$'p | uniq
+	echo ""
 }
 
 # bookmark a folder
-function bm {
+function bookmark_add {
 	apparix --add-mark $1
 }
 
-## to bookmark
-function bt {
-	folder=$(apparix $1)
-	cd $folder
+# to bookmark
+function bookmark_to {
+	if [[ $# -gt 0 ]]; then
+		folder=$(apparix $1)
+		cd $folder
+		ls $folder
+	else
+		bookmark_help
+	fi	
 }
 
-_bt_complete() {
+# auto complete
+_bookmark_complete() {
 	A=$(apparix | sed -n '4,$'p  | awk '{ print $2 }')
 	_arguments ":::($A)"
 }
 
-compdef _bt_complete bt
+compdef _bookmark_complete bookmark_to
+
+alias bt=bookmark_to
+alias bl=bookmark_list
+alias ba=bookmark_add
